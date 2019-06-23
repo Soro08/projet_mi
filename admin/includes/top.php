5b2a36dc60eaf62958df259727c6d8f31ba64300
Soro08 <soro@nan.ci>
2019-06-23 04:44:18 +0200

    initialisation de ka partie admin

---
+++ admin/includes/top.php
@@ -0,0 +1,36 @@
+<!-- Basic -->
+<meta charset="UTF-8">
+
+<title>Dashboard | JSOFT Themes | JSOFT-Admin</title>
+<meta name="keywords" content="HTML5 Admin Template" />
+<meta name="description" content="JSOFT Admin - Responsive HTML5 Template">
+<meta name="author" content="JSOFT.net">
+
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+<!-- Web Fonts  -->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+
+<!-- Vendor CSS -->
+<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.css" />
+<link rel="stylesheet" href="assets/vendor/magnific-popup/magnific-popup.css" />
+<link rel="stylesheet" href="assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+
+<!-- Specific Page Vendor CSS -->
+<link rel="stylesheet" href="assets/vendor/jquery-ui/css/ui-lightness/jquery-ui-1.10.4.custom.css" />
+<link rel="stylesheet" href="assets/vendor/bootstrap-multiselect/bootstrap-multiselect.css" />
+<link rel="stylesheet" href="assets/vendor/morris/morris.css" />
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="assets/stylesheets/theme.css" />
+
+<!-- Skin CSS -->
+<link rel="stylesheet" href="assets/stylesheets/skins/default.css" />
+
+<!-- Theme Custom CSS -->
+<link rel="stylesheet" href="assets/stylesheets/theme-custom.css">
+
+<!-- Head Libs -->
+<script src="assets/vendor/modernizr/modernizr.js"></script>
